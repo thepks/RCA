f7d623cec22e6e04b3c52cfb58320140f3d7071d
thepks <thepksgp@gmail.com>
2015-04-18 15:30:38 +0100

    update to node to make it more generic

--- node/index.js
+++ node/index.js
@@ -3,10 +3,16 @@ var forge = require('node-forge');
 var bodyParser = require('body-parser');
 var session = require('cookie-session');
 var http = require('http');
-var service_config = require('hidden.js');
 var app = express();
 
 
+var service_config = {
+    'host': 'rca1.sb04.stations.graphenedb.com',
+    'port': 24789,
+    'path': '/db/data/transaction/commit',
+    'username': 'rca1',
+    'password': ''
+};
 
 var jsonParser = bodyParser.json();
 
@@ -24,14 +30,31 @@ var form_post_headers = function(username) {
     };
 };
 
-var form_proxy_headers = function(body) {
+var form_proxy_options = function(req) {
+
+    //    console.log(JSON.stringify(req.headers));
+
+    var new_headers = {};
+    if ('content-type' in req.headers) {
+        new_headers['content-type'] = req.headers['content-type'];
+    }
+    if ('content-length' in req.headers) {
+        new_headers['content-length'] = req.headers['content-length'];
+    }
+    new_headers['Authorization'] = 'Basic ' + new Buffer(service_config.username + ":" + service_config.password).toString('base64');
+
+
+
     return {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(body, 'utf8'),
+        hostname: service_config.host,
+        port: service_config.port,
+        path: req.url,
+        method: req.method,
         'X-Stream': true,
-        'Authorization': 'Basic ' + new Buffer(service_config.username + ":" + service_config.password).toString('base64')
+        headers: new_headers
     };
 };
+
 
 var form_post_options = function(username) {
 
@@ -46,19 +69,6 @@ var form_post_options = function(username) {
 };
 
 
-var form_proxy_options = function(body) {
-
-    return {
-        host: service_config.host,
-        port: service_config.port,
-        path: service_config.path,
-        method: 'POST',
-        headers: form_proxy_headers(body)
-    };
-};
-
-
-
 app.use(express.static('/home/action/RCA/'));
 
 app.use(session({
@@ -66,7 +76,7 @@ app.use(session({
 }));
 
 
-app.all('/db/data/transaction/commit', jsonParser, function(req, res) {
+app.all('/db/data/transaction/commit', function(req, res) {
 
 
     if (!req.session.auth) {
@@ -75,113 +85,125 @@ app.all('/db/data/transaction/commit', jsonParser, function(req, res) {
         return false;
     }
 
-    var origBody = JSON.stringify(req.body);
-    console.log(origBody);
 
     // do the POST call
-    var reqPost = http.request(form_proxy_options(origBody), function(resembedded) {
+    var reqPost = http.request(form_proxy_options(req), function(resembedded) {
         console.log("statusCode: ", resembedded.statusCode);
         // uncomment it for header details
         //  console.log("headers: ", res.headers);
-        
+
         resembedded.pipe(res);
 
     });
+
+    req.addListener('data', function(d) {
+        //    console.log(d);
+        reqPost.write(d, 'binary');
+    });
+
+    req.addListener('end', function(d) {
+        reqPost.end();
+    });
+
+    reqPost.on('error', function(e) {
+        console.error(e);
+        res.statusCode = 500;
+        res.send('Error ' + e);
+    });
+
+
+
+});
+
+
+
+app.all('/action/logoff', function(req, res) {
+    console.log('Logged on? ' + req.session.auth);
+    req.session.auth = false;
+    req.session.roles = [];
+    res.statusCode = 200;
+    res.send("Logged off");
+});
+
+app.post('/action/logon', jsonParser, function(req, res) {
+    var secret;
+    var md = forge.md.sha1.create();
+    var reply_body = '';
+    md.update(req.body.user.password);
+    secret = md.digest().toHex();
+    console.log(secret);
+    console.log(form_user_search(req.body.user.username));
+
+
+    // do the POST call
+    var reqPost = http.request(form_post_options(req.body.user.username), function(resembedded) {
+        console.log("statusCode: ", resembedded.statusCode);
+        // uncomment it for header details
+        //  console.log("headers: ", res.headers);
+
+        resembedded.on('data', function(d) {
+            reply_body += d;
+        });
+
+        resembedded.on('end', function(d) {
+
+            var result = {};
+            console.info('POST result:\n');
+            process.stdout.write(reply_body);
+            console.info('\n\nPOST completed');
+            var p = JSON.parse(reply_body);
+            try {
+                if (p.results[0].data[0].row[0] === secret) {
+
+                    result.authorised = true;
+                    result.roles = [];
+
+                    req.session.auth = true;
+                    req.session.roles = [];
+                    res.statusCode = 200;
+                    for (var j = 0; j < p.results[0].data.length; j++) {
+                        console.log('Permission: ' + p.results[0].data[j].row[2]);
+                        req.session.roles.push(p.results[0].data[j].row[2]);
+                        result.roles.push(p.results[0].data[j].row[2]);
+                    }
+                    res.send(JSON.stringify(result));
+                } else {
+                    req.session.auth = false;
+                    res.statusCode = 401;
+                    result.authorised = false;
+                    res.send(JSON.stringify(result));
+                }
+
+            } catch (e) {
+                console.log('Failed to find path');
+                req.session.auth = false;
+                req.session.roles = [];
+                res.statusCode = 401;
+                result.authorised = false;
+                res.send(JSON.stringify(result));
+            }
+
+
+        });
+    });
+
     // write the json data
-    reqPost.write(origBody);
+    var posting = form_user_search(req.body.user.username);
+    console.log(posting);
+    reqPost.write(posting);
     reqPost.end();
     reqPost.on('error', function(e) {
         console.error(e);
     });
 
-    
 });
 
 
+var server = app.listen(8080, function() {
 
-        app.all('/action/logoff', function(req, res) {
+    var host = server.address().address;
+    var port = server.address().port;
 
-            console.log('Logged on? ' + req.session.auth);
-            req.session.auth = false;
-            req.session.roles = [];
-            res.statusCode = 200;
-            res.send("Logged off");
+    console.log('Server app listening at http://%s:%s', host, port);
 
-        });
-
-        app.post('/action/logon', jsonParser, function(req, res) {
-            var secret;
-            var md = forge.md.sha1.create();
-            md.update(req.body.user.password);
-            secret = md.digest().toHex();
-            console.log(secret);
-            console.log(form_user_search(req.body.user.username));
-
-
-            // do the POST call
-            var reqPost = http.request(form_post_options(req.body.user.username), function(resembedded) {
-                console.log("statusCode: ", resembedded.statusCode);
-                // uncomment it for header details
-                //  console.log("headers: ", res.headers);
-
-                resembedded.on('data', function(d) {
-                    var result = {};
-                    console.info('POST result:\n');
-                    process.stdout.write(d);
-                    console.info('\n\nPOST completed');
-                    var p = JSON.parse(d);
-                    try {
-                        if (p.results[0].data[0].row[0] === secret) {
-
-                            result.authorised = true;
-                            result.roles = [];
-
-                            req.session.auth = true;
-                            req.session.roles = [];
-                            res.statusCode = 200;
-                            for (var j = 0; j < p.results[0].data.length; j++) {
-                                console.log('Permission: ' + p.results[0].data[j].row[2]);
-                                req.session.roles.push(p.results[0].data[j].row[2]);
-                                result.roles.push(p.results[0].data[j].row[2]);
-                            }
-                            res.send(JSON.stringify(result));
-                        } else {
-                            req.session.auth = false;
-                            res.statusCode = 401;
-                            result.authorised = false;
-                            res.send(JSON.stringify(result));
-                        }
-
-                    } catch (e) {
-                        console.log('Failed to find path');
-                        req.session.auth = false;
-                        req.session.roles = [];
-                        res.statusCode = 401;
-                        result.authorised = false;
-                        res.send(JSON.stringify(result));
-                    }
-
-
-                });
-            });
-
-            // write the json data
-            var posting = form_user_search(req.body.user.username);
-            console.log(posting);
-            reqPost.write(posting);
-            reqPost.end();
-            reqPost.on('error', function(e) {
-                console.error(e);
-            });
-
-        });
-
-
-        var server = app.listen(8080, function() {
-
-            var host = server.address().address;
-            var port = server.address().port;
-
-            console.log('Server app listening at http://%s:%s', host, port);
-
-        });
+});
